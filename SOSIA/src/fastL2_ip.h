fe0ad733f1f56b0954434f63dd05066bac9ce9a9
bianzheng123 <1317257555@qq.com>
2024-10-22 20:40:18 +0800

    reformat the code

--- SOSIA/src/fastL2_ip.h
+++ SOSIA/src/fastL2_ip.h
@@ -4,7 +4,6 @@
 #define USE_FAST
 
 
-
 #ifdef USE_FAST
 
 #define __SSE__
@@ -23,7 +22,9 @@
 #include <intrin.h>
 #include <stdexcept>
 #else
+
 #include <x86intrin.h>
+
 #endif
 
 #if defined(__GNUC__)
@@ -36,7 +37,7 @@
 namespace fastlib {
 
     static float
-        L2Sqr(float* pVect1, float* pVect2, size_t qty) {
+    L2Sqr(float *pVect1, float *pVect2, size_t qty) {
         float res = 0;
         for (size_t i = 0; i < qty; i++) {
             float t = *pVect1 - *pVect2;
@@ -48,7 +49,7 @@ namespace fastlib {
     }
 
     static float
-        InnerProduct(float* pVect1, float* pVect2, size_t qty) {
+    InnerProduct(float *pVect1, float *pVect2, size_t qty) {
         float res = 0;
         for (size_t i = 0; i < qty; i++) {
             res += *pVect1 * (*pVect2);
@@ -62,11 +63,11 @@ namespace fastlib {
 
     // Favor using AVX if available.
     static float
-        L2SqrSIMD16Ext(float* pVect1, float* pVect2, size_t qty) {
+    L2SqrSIMD16Ext(float *pVect1, float *pVect2, size_t qty) {
         float PORTABLE_ALIGN32 TmpRes[8];
         size_t qty16 = qty >> 4;
 
-        const float* pEnd1 = pVect1 + (qty16 << 4);
+        const float *pEnd1 = pVect1 + (qty16 << 4);
 
         __m256 diff, v1, v2;
         __m256 sum = _mm256_set1_ps(0);
@@ -92,11 +93,11 @@ namespace fastlib {
     }
 
     static float
-        IpSIMD16Ext(float* pVect1, float* pVect2, size_t qty) {
+    IpSIMD16Ext(float *pVect1, float *pVect2, size_t qty) {
         float PORTABLE_ALIGN32 TmpRes[8];
         size_t qty16 = qty >> 4;
 
-        const float* pEnd1 = pVect1 + (qty16 << 4);
+        const float *pEnd1 = pVect1 + (qty16 << 4);
 
         __m256 v1, v2;
         __m256 sum = _mm256_set1_ps(0);
@@ -120,16 +121,18 @@ namespace fastlib {
         _mm256_store_ps(TmpRes, sum);
         return TmpRes[0] + TmpRes[1] + TmpRes[2] + TmpRes[3] + TmpRes[4] + TmpRes[5] + TmpRes[6] + TmpRes[7];
     }
+
 #endif
 
 #if defined(USE_SSE) || defined(USE_AVX)
+
     static float
-        L2SqrSIMD16ExtResiduals(float* pVect1v, float* pVect2v, size_t qty) {
+    L2SqrSIMD16ExtResiduals(float *pVect1v, float *pVect2v, size_t qty) {
         //size_t qty = *((size_t*)qty_ptr);
         size_t qty16 = qty >> 4 << 4;
         float res = L2SqrSIMD16Ext(pVect1v, pVect2v, qty16);
-        float* pVect1 = (float*)pVect1v + qty16;
-        float* pVect2 = (float*)pVect2v + qty16;
+        float *pVect1 = (float *) pVect1v + qty16;
+        float *pVect2 = (float *) pVect2v + qty16;
 
         size_t qty_left = qty - qty16;
         float res_tail = L2Sqr(pVect1, pVect2, qty_left);
@@ -137,28 +140,30 @@ namespace fastlib {
     }
 
     static float
-        IpSIMD16ExtResiduals(float* pVect1v, float* pVect2v, size_t qty) {
+    IpSIMD16ExtResiduals(float *pVect1v, float *pVect2v, size_t qty) {
         //size_t qty = *((size_t*)qty_ptr);
         size_t qty16 = qty >> 4 << 4;
         float res = IpSIMD16Ext(pVect1v, pVect2v, qty16);
-        float* pVect1 = (float*)pVect1v + qty16;
-        float* pVect2 = (float*)pVect2v + qty16;
+        float *pVect1 = (float *) pVect1v + qty16;
+        float *pVect2 = (float *) pVect2v + qty16;
 
         size_t qty_left = qty - qty16;
         float res_tail = InnerProduct(pVect1, pVect2, qty_left);
         return (res + res_tail);
     }
+
 #endif
 
 
 #ifdef USE_SSE
+
     static float
-        L2SqrSIMD4Ext(float* pVect1, float* pVect2, size_t qty) {
+    L2SqrSIMD4Ext(float *pVect1, float *pVect2, size_t qty) {
         float PORTABLE_ALIGN32 TmpRes[8];
 
         size_t qty4 = qty >> 2;
 
-        const float* pEnd1 = pVect1 + (qty4 << 2);
+        const float *pEnd1 = pVect1 + (qty4 << 2);
 
         __m128 diff, v1, v2;
         __m128 sum = _mm_set1_ps(0);
@@ -176,7 +181,7 @@ namespace fastlib {
     }
 
     static float
-        IpSIMD4Ext(float* pVect1, float* pVect2, size_t qty) {
+    IpSIMD4Ext(float *pVect1, float *pVect2, size_t qty) {
         float PORTABLE_ALIGN32 TmpRes[8];
         //float* pVect1 = (float*)pVect1v;
         //float* pVect2 = (float*)pVect2v;
@@ -185,7 +190,7 @@ namespace fastlib {
 
         size_t qty4 = qty >> 2;
 
-        const float* pEnd1 = pVect1 + (qty4 << 2);
+        const float *pEnd1 = pVect1 + (qty4 << 2);
 
         __m128 v1, v2;
         __m128 sum = _mm_set1_ps(0);
@@ -203,40 +208,40 @@ namespace fastlib {
     }
 
     static float
-        L2SqrSIMD4ExtResiduals(float* pVect1v, float* pVect2v, size_t qty) {
+    L2SqrSIMD4ExtResiduals(float *pVect1v, float *pVect2v, size_t qty) {
         //size_t qty = *((size_t*)qty_ptr);
         size_t qty4 = qty >> 2 << 2;
 
         float res = L2SqrSIMD4Ext(pVect1v, pVect2v, qty4);
         size_t qty_left = qty - qty4;
 
-        float* pVect1 = (float*)pVect1v + qty4;
-        float* pVect2 = (float*)pVect2v + qty4;
+        float *pVect1 = (float *) pVect1v + qty4;
+        float *pVect2 = (float *) pVect2v + qty4;
         float res_tail = L2Sqr(pVect1, pVect2, qty_left);
 
         return (res + res_tail);
     }
 
     static float
-        IpSIMD4ExtResiduals(float* pVect1v, float* pVect2v, size_t qty) {
+    IpSIMD4ExtResiduals(float *pVect1v, float *pVect2v, size_t qty) {
         //size_t qty = *((size_t*)qty_ptr);
         size_t qty4 = qty >> 2 << 2;
 
         float res = IpSIMD4Ext(pVect1v, pVect2v, qty4);
         size_t qty_left = qty - qty4;
 
-        float* pVect1 = (float*)pVect1v + qty4;
-        float* pVect2 = (float*)pVect2v + qty4;
+        float *pVect1 = (float *) pVect1v + qty4;
+        float *pVect2 = (float *) pVect2v + qty4;
         float res_tail = InnerProduct(pVect1, pVect2, qty_left);
 
         return (res + res_tail);
     }
+
 #endif
 
 }
 
-inline float calL2Sqr_fast(float* v1, float* v2, int dim)
-{
+inline float calL2Sqr_fast(float *v1, float *v2, int dim) {
 #if defined(USE_FAST)
     if (dim % 16 == 0)
         return fastlib::L2SqrSIMD16Ext(v1, v2, dim);
@@ -257,8 +262,7 @@ inline float calL2Sqr_fast(float* v1, float* v2, int dim)
 
 }
 
-inline float calIp_fast(float* v1, float* v2, int dim)
-{
+inline float calIp_fast(float *v1, float *v2, int dim) {
 #if defined(USE_FAST)
     if (dim % 16 == 0)
         return fastlib::IpSIMD16Ext(v1, v2, dim);
@@ -281,10 +285,10 @@ inline float calIp_fast(float* v1, float* v2, int dim)
 
 namespace fastlib1 {
     static float
-        L2Sqr(const void* pVect1v, const void* pVect2v, const void* qty_ptr) {
-        float* pVect1 = (float*)pVect1v;
-        float* pVect2 = (float*)pVect2v;
-        size_t qty = *((size_t*)qty_ptr);
+    L2Sqr(const void *pVect1v, const void *pVect2v, const void *qty_ptr) {
+        float *pVect1 = (float *) pVect1v;
+        float *pVect2 = (float *) pVect2v;
+        size_t qty = *((size_t *) qty_ptr);
 
         float res = 0;
         for (size_t i = 0; i < qty; i++) {
@@ -300,14 +304,14 @@ namespace fastlib1 {
 
     // Favor using AVX if available.
     static float
-        L2SqrSIMD16Ext(const void* pVect1v, const void* pVect2v, const void* qty_ptr) {
-        float* pVect1 = (float*)pVect1v;
-        float* pVect2 = (float*)pVect2v;
-        size_t qty = *((size_t*)qty_ptr);
+    L2SqrSIMD16Ext(const void *pVect1v, const void *pVect2v, const void *qty_ptr) {
+        float *pVect1 = (float *) pVect1v;
+        float *pVect2 = (float *) pVect2v;
+        size_t qty = *((size_t *) qty_ptr);
         float PORTABLE_ALIGN32 TmpRes[8];
         size_t qty16 = qty >> 4;
 
-        const float* pEnd1 = pVect1 + (qty16 << 4);
+        const float *pEnd1 = pVect1 + (qty16 << 4);
 
         __m256 diff, v1, v2;
         __m256 sum = _mm256_set1_ps(0);
@@ -384,33 +388,36 @@ namespace fastlib1 {
 #endif
 
 #if defined(USE_SSE) || defined(USE_AVX)
+
     static float
-        L2SqrSIMD16ExtResiduals(const void* pVect1v, const void* pVect2v, const void* qty_ptr) {
-        size_t qty = *((size_t*)qty_ptr);
+    L2SqrSIMD16ExtResiduals(const void *pVect1v, const void *pVect2v, const void *qty_ptr) {
+        size_t qty = *((size_t *) qty_ptr);
         size_t qty16 = qty >> 4 << 4;
         float res = L2SqrSIMD16Ext(pVect1v, pVect2v, &qty16);
-        float* pVect1 = (float*)pVect1v + qty16;
-        float* pVect2 = (float*)pVect2v + qty16;
+        float *pVect1 = (float *) pVect1v + qty16;
+        float *pVect2 = (float *) pVect2v + qty16;
 
         size_t qty_left = qty - qty16;
         float res_tail = L2Sqr(pVect1, pVect2, &qty_left);
         return (res + res_tail);
     }
+
 #endif
 
 
 #ifdef USE_SSE
+
     static float
-        L2SqrSIMD4Ext(const void* pVect1v, const void* pVect2v, const void* qty_ptr) {
+    L2SqrSIMD4Ext(const void *pVect1v, const void *pVect2v, const void *qty_ptr) {
         float PORTABLE_ALIGN32 TmpRes[8];
-        float* pVect1 = (float*)pVect1v;
-        float* pVect2 = (float*)pVect2v;
-        size_t qty = *((size_t*)qty_ptr);
+        float *pVect1 = (float *) pVect1v;
+        float *pVect2 = (float *) pVect2v;
+        size_t qty = *((size_t *) qty_ptr);
 
 
         size_t qty4 = qty >> 2;
 
-        const float* pEnd1 = pVect1 + (qty4 << 2);
+        const float *pEnd1 = pVect1 + (qty4 << 2);
 
         __m128 diff, v1, v2;
         __m128 sum = _mm_set1_ps(0);
@@ -428,24 +435,24 @@ namespace fastlib1 {
     }
 
     static float
-        L2SqrSIMD4ExtResiduals(const void* pVect1v, const void* pVect2v, const void* qty_ptr) {
-        size_t qty = *((size_t*)qty_ptr);
+    L2SqrSIMD4ExtResiduals(const void *pVect1v, const void *pVect2v, const void *qty_ptr) {
+        size_t qty = *((size_t *) qty_ptr);
         size_t qty4 = qty >> 2 << 2;
 
         float res = L2SqrSIMD4Ext(pVect1v, pVect2v, &qty4);
         size_t qty_left = qty - qty4;
 
-        float* pVect1 = (float*)pVect1v + qty4;
-        float* pVect2 = (float*)pVect2v + qty4;
+        float *pVect1 = (float *) pVect1v + qty4;
+        float *pVect2 = (float *) pVect2v + qty4;
         float res_tail = L2Sqr(pVect1, pVect2, &qty_left);
 
         return (res + res_tail);
     }
+
 #endif
 }
 
-inline float calL2Sqr_fast1(float* v1, float* v2, size_t dim)
-{
+inline float calL2Sqr_fast1(float *v1, float *v2, size_t dim) {
 #if defined(USE_FAST)
     if (dim % 16 == 0)
         return fastlib1::L2SqrSIMD16Ext(v1, v2, &dim);
--- SOSIA/src/main.cpp
+++ SOSIA/src/main.cpp
@@ -27,96 +27,60 @@ std::uniform_real_distribution<float> mips2set::ur(0, 1);
 
 std::string getNameByDate();
 
-//Load VBMW Data
-using dt = uint32_t;
-void loadVBMW_dataset(const std::string& path)
-{
-	dt* res = nullptr;
-	std::ifstream in(path.c_str(), std::ios::binary);
-	if (!in) {
-		std::cout << "Fail to open the file:\n" << path << "!\n";
-		exit(-10086);
-	}
-	//dt n=0;
-	//in.read((char*)&n, sizeof(dt));
-	//std::cout << "n= [" << n << "]: " << std::endl;
-	dt* tmp = new dt[100000];
-	int cnt = 0;
-	std::vector<dt> lens;
-	while (!in.eof()) {
-		int len = 0;
-		in.read((char*)&len, sizeof(dt));
-		in.read((char*)tmp, sizeof(dt) * (len));
-		lens.push_back(len);
-		if (cnt == 0) {
-			std::cout << cnt++ << "-th posting list [" << len << "]: " << std::endl;
-			for (int i = 0; i < len; ++i) {
-				std::cout << tmp[i] << " ";
-			}
-			std::cout << std::endl;
-		}
+int main(int argc, char const *argv[]) {
+    rdCoutStream rd(
+            getNameByDate()); //Redirect the cout to a file and output it to the console at the same time (Create at Nov 13, 2023)
 
-		cnt++;
-		
-	}
-	std::cout << "cnt= [" << cnt << "]: " << std::endl;
-	return;
-}
+    std::string dataset = "base_small";
+    std::string qname = "queries";
 
-int main(int argc, char const* argv[])
-{
-	rdCoutStream rd(getNameByDate()); //Redirect the cout to a file and output it to the console at the same time (Create at Nov 13, 2023)
+    int datasetID = -1;
 
-	std::string dataset = "base_small";
-	std::string qname= "queries";
+    int L = 5;
+    int K = 12;
+    int k = 50;
+    float c = 0.5;
+    int l = 20;
+    int ub = 10000;
+    int m = 150;
+    int b = 1;
+    float F = 2.0f;
+    int sin_m = 30, sin_h = 2;
+    int mode = 0;
 
-	int datasetID = -1;
-	
-	int L = 5;
-	int K = 12;
-	int k = 50;
-	float c = 0.5;
-	int l = 20;
-	int ub = 10000;
-	int m = 150;
-	int b = 1;
-	float F = 2.0f;
-	int sin_m = 30, sin_h = 2;
-	int mode = 0;
+    if (argc > 1) mode = std::atoi(argv[1]);
+    if (argc > 2) dataset = argv[2];
+    if (argc > 3) qname = argv[3];
 
-	if (argc > 1) mode = std::atoi(argv[1]);
-	if (argc > 2) dataset = argv[2];
-	if (argc > 3) qname = argv[3];
-	
-	//if (argc > 4) l = std::atoi(argv[3]);
+    //if (argc > 4) l = std::atoi(argv[3]);
 
-	std::string argvStr[5];
-	argvStr[1] = "datasets/" + dataset + ".csr";
-	argvStr[2] = (dataset + ".index");
-	argvStr[3] = ("datasets/"+dataset + "_all.ben");
-	argvStr[4] = "datasets/" + qname + ".dev.csr";
+    std::string argvStr[5];
+    argvStr[1] = "datasets/" + dataset + ".csr";
+    argvStr[2] = (dataset + ".index");
+    argvStr[3] = ("datasets/" + dataset + "_all.ben");
+    argvStr[4] = "datasets/" + qname + ".dev.csr";
 
-	std::cout << "Using SOSIA for Sparse Dataset " << argvStr[1] << std::endl;
-	Preprocess prep((argvStr[1]), (argvStr[4]), (argvStr[3]));
-	std::vector<resOutput> res;
-	switch(mode){
-		case 0:
-			runAll(prep, c, k, m, ub, F, l, sin_m, sin_h, res);
-			break;
-		case 1:
-			varyL(prep, c, k, m, ub, F, l, sin_m, sin_h, res);
-			break;
-		case 2:
-			varyM(prep, c, k, m, ub, F, l, sin_m, sin_h, res);
-			break;
-		case 3:
-			varyk(prep, c, k, m, ub, F, l, sin_m, sin_h, res);
-			break;	
-		case 4:
-			RecallTime(prep, c, k, m, ub, F, l, sin_m, sin_h, res);
-			break;
-	}
+    std::cout << "Using SOSIA for Sparse Dataset " << argvStr[1] << std::endl;
+    Preprocess prep((argvStr[1]), (argvStr[4]), (argvStr[3]));
+    std::vector<resOutput> res;
+    switch (mode) {
+        case 0:
+            runAll(prep, c, k, m, ub, F, l, sin_m, sin_h, res);
+            break;
+        case 1:
+            varyL(prep, c, k, m, ub, F, l, sin_m, sin_h, res);
+            break;
+        case 2:
+            varyM(prep, c, k, m, ub, F, l, sin_m, sin_h, res);
+            break;
+        case 3:
+            varyk(prep, c, k, m, ub, F, l, sin_m, sin_h, res);
+            break;
+        case 4:
+            RecallTime(prep, c, k, m, ub, F, l, sin_m, sin_h, res);
+            break;
+    }
 
-	saveAndShow(c, k, dataset, res);
-	return 0;
+    saveAndShow(c, k, dataset, res);
+    return 0;
 }
